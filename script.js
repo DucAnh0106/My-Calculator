254e65b2d9930ebc1737c918d8765a6600610db6
DucAnh0106 <233656241+DucAnh0106@users.noreply.github.com>
2026-02-02 21:23:29 +0800

    Resize padding for calculator body and remove JS code to seperate UI with the logic

--- script.js
+++ script.js
@@ -1,36 +1,4 @@
-function addNumbers(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
-}
 
-function subtractNumbers(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-}
-
-function divideNumbers(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
-}
-
-function multiplyNumbers(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
-
-function operation(firstNumber, secondNumber, secondNumber) {
-    if (operator === '+') {
-        return addNumbers(firstNumber, secondNumber);
-    } else if (operator === '-') {
-        return subtractNumbers(firstNumber, secondNumber);
-    } else if (operator === '*') {
-        return multiplyNumbers(firstNumber, secondNumber);
-    } else {
-        return divideNumbers(firstNumber, secondNumber);
-}
-}
-
-let firstNumber = Number( prompt('Enter first number','') );
-let operator = prompt('Enter +, -, * or /','');
-let secondNumber = Number( prompt('Enter second number','') );
-
-console.log ( operation(firstNumber, operator, secondNumber) );
 
 
 
