a55b86a27815835ae0c650f47f8eb0ef79c214d0
DucAnh0106 <233656241+DucAnh0106@users.noreply.github.com>
2026-02-06 15:51:50 +0800

    implement new logic to seperate 2 user intents: Edit Mode and Finalize Mode Edit mode: when user is still considering what is the first Number Finalize mode: when user is sure about what he/she is gonna put for the number, he/she will assign the Input buffer to that first/second number

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 function addNumbers(firstNumber, secondNumber) {
     return firstNumber + secondNumber;
 }
-
 function subtractNumbers(firstNumber, secondNumber) {
     return firstNumber - secondNumber;
 }
-
 function divideNumbers(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
-
 function multiplyNumbers(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
-
 function operation(firstNumber, operator, secondNumber) {
     if (operator === '+') {
         return addNumbers(firstNumber, secondNumber);
@@ -30,66 +26,42 @@ let firstNumber;
 let operator;
 let secondNumber;
 
-let firstNumberStorage = '';
-let secondNumberStorage = '';
+let userInputBuffer = '';
 
 //target text box
 const textBox = document.querySelector('#textBox');
 let textBoxDisplay = document.createElement('span');
-
 textBox.appendChild(textBoxDisplay);
 
 //target buttons
 const numberButtons = document.querySelectorAll('.numberButtons');
 
-//when we are expecting to add digits to firstNumber, status is true
-//else, status is false
-let inputStatus = true;
-
-
-const displayOnScreen = function(event) {
-    if ( inputStatus === true ) {
-        textBoxDisplay.textContent = firstNumberStorage;
-    } else {
-        textBoxDisplay.textContent = secondNumberStorage;
-    }
+const displayOnScreen = function() {
+    textBoxDisplay.textContent = userInputBuffer;
 }
 
-const updateNumber = function (event) {
-    let currentDisplayText = textBoxDisplay.textContent;
-    console.log(`Current text: ${textBoxDisplay.textContent}`);
-    //If current text is not an operator
-    //it means we're still inputting the first Number
-    if ( inputStatus === false ) { 
-        secondNumberStorage += event.target.textContent;
-        secondNumber = Number(secondNumberStorage);
-        console.log(`Current second Number: ${secondNumber}`);
-    } else {
-        firstNumberStorage += event.target.textContent;
-        firstNumber = Number(firstNumberStorage); 
-        console.log(`Current first Number: ${firstNumber}`);
-    }
-
+const editNumber = function (event) {
+    userInputBuffer += event.target.textContent;
     displayOnScreen(event);
 }
 
 numberButtons.forEach( button => {
-    button.addEventListener('click', updateNumber);
+    button.addEventListener('click', editNumber);
 });
 
 //target operators
 const operators = document.querySelectorAll('.operators');
 
-const updateOperator = function(event) {
+const chooseOperator = function(event) {
     operator = event.target.textContent;
+    textBoxDisplay.textContent = operator;
 
-    textBoxDisplay.textContent = event.target.textContent;
-    inputStatus = false;
-    console.log(`Current text: ${textBoxDisplay.textContent}`);
+    firstNumber = Number( userInputBuffer );
+    userInputBuffer = ''; //Switch back to edit mode
 }
 
 operators.forEach( operator => {
-    operator.addEventListener('click', updateOperator);
+    operator.addEventListener('click', chooseOperator);
 })
 
 //target equal button
@@ -97,14 +69,13 @@ const equalButton = document.querySelector('#equalButton');
 let result;
 
 equalButton.addEventListener('click', (event) => {
+    secondNumber = Number(userInputBuffer); // finalize mode for second Num
+
     result = operation(firstNumber, operator, secondNumber);
     textBoxDisplay.textContent = result;
 
-    //this is to reset the states back to input firstNumber 
-    inputStatus = true;
-    firstNumber = result;
-    //firstNumberStorage = '' but no need cause it's already a result
-    secondNumberStorage = '' //reset back to nothing to wait for another number to perform operations on
+    firstNumber = result; //to chain operations
+    userInputBuffer = ''; //reset to edit mode
 })
 
 
